156ded49af41e7b0219e50c7289c5cb0d0384645
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2022-04-19 20:45:24 +0200

    skript vytvoreni orezanych disku v pythonu

--- skript_creation_of_crop_images_with_disc_detector.py
+++ skript_creation_of_crop_images_with_disc_detector.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
         plt.imsave(path_to_crop_disc + files_mask_disc[i][59:],output_mask_disc)
         plt.imsave(path_to_crop_cup + files_mask_cup[i][58:],output_mask_cup)
          
-    '''
+    
     # Riga - Bin Rushed
     pocet_train=0.8
     
@@ -165,7 +165,255 @@ if __name__ == "__main__":
             plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][64:],mask_disc)
             plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][63:],mask_cup)
             
+          
+    # Riga - Magrabia
+    pocet_train=0.8
     
-
-
+    files_img=glob.glob(path_to_data+'/RIGA/Images/Magrabia/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/RIGA/Disc/Magrabia/expert1/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/RIGA/Cup/Magrabia/expert1/*.png')
+    files_fov=glob.glob(path_to_data+'/RIGA/FOV/Magrabia/*.png')
+    path_to_crop_image=path_to_data+'/RIGA/Images_crop/'
+    path_to_crop_disc=path_to_data+'/RIGA/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/RIGA/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][57:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][63:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][62:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][57:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][63:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][62:],mask_cup)
+            
+    
+    # Riga - MESSIDOR
+    pocet_train=0.8
+    
+    files_img=glob.glob(path_to_data+'/RIGA/Images/MESSIDOR/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/RIGA/Disc/MESSIDOR/expert1/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/RIGA/Cup/MESSIDOR/expert1/*.png')
+    files_fov=glob.glob(path_to_data+'/RIGA/FOV/MESSIDOR/*.png')
+    path_to_crop_image=path_to_data+'/RIGA/Images_crop/'
+    path_to_crop_disc=path_to_data+'/RIGA/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/RIGA/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][57:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][63:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][62:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][57:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][63:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][62:],mask_cup)
+    
+    
+    # RIM-ONE - glaucoma
+    pocet_train=0.8
+    
+    files_img=glob.glob(path_to_data+'/RIM-ONE/Images/Glaucoma/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/RIM-ONE/Disc/Glaucoma/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/RIM-ONE/Cup/Glaucoma/*.png')
+    files_fov=glob.glob(path_to_data+'/RIM-ONE/FOV/Glaucoma/*.png')
+    path_to_crop_image=path_to_data+'/RIM-ONE/Images_crop/'
+    path_to_crop_disc=path_to_data+'/RIM-ONE/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/RIM-ONE/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][60:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][58:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][57:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][60:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][58:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][57:],mask_cup)
+     
+             
+    # RIM-ONE - Healthy
+    pocet_train=0.8
+    
+    files_img=glob.glob(path_to_data+'/RIM-ONE/Images/Healthy/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/RIM-ONE/Disc/Healthy/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/RIM-ONE/Cup/Healthy/*.png')
+    files_fov=glob.glob(path_to_data+'/RIM-ONE/FOV/Healthy/*.png')
+    path_to_crop_image=path_to_data+'/RIM-ONE/Images_crop/'
+    path_to_crop_disc=path_to_data+'/RIM-ONE/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/RIM-ONE/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][59:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][57:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][56:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][59:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][57:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][56:],mask_cup)
+    '''
+    # UOA-DR - Healthy
+    pocet_train=0.8
+    
+    files_img=glob.glob(path_to_data+'/UoA_DR/Images/Healthy/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/UoA_DR/Disc/Healthy/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/UoA_DR/Cup/Healthy/*.png')
+    files_fov=glob.glob(path_to_data+'/UoA_DR/FOV/Healthy/*.png')
+    path_to_crop_image=path_to_data+'/UoA_DR/Images_crop/'
+    path_to_crop_disc=path_to_data+'/UoA_DR/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/UoA_DR/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][58:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][56:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][55:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][58:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][56:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][55:],mask_cup)
  
+    
+    # UOA-DR - NDPR
+    pocet_train=0.8
+    
+    files_img=glob.glob(path_to_data+'/UoA_DR/Images/NDPR/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/UoA_DR/Disc/NDPR/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/UoA_DR/Cup/NDPR/*.png')
+    files_fov=glob.glob(path_to_data+'/UoA_DR/FOV/NDPR/*.png')
+    path_to_crop_image=path_to_data+'/UoA_DR/Images_crop/'
+    path_to_crop_disc=path_to_data+'/UoA_DR/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/UoA_DR/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][55:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][53:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][52:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][55:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][53:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][52:],mask_cup)
+    
+    # UOA-DR - PDR
+    pocet_train=0.8
+    
+    files_img=glob.glob(path_to_data+'/UoA_DR/Images/PDR/*.png')
+    files_mask_disc=glob.glob(path_to_data+'/UoA_DR/Disc/PDR/*.png')
+    files_mask_cup=glob.glob(path_to_data+'/UoA_DR/Cup/PDR/*.png')
+    files_fov=glob.glob(path_to_data+'/UoA_DR/FOV/PDR/*.png')
+    path_to_crop_image=path_to_data+'/UoA_DR/Images_crop/'
+    path_to_crop_disc=path_to_data+'/UoA_DR/Disc_crop/'
+    path_to_crop_cup=path_to_data+'/UoA_DR/Cup_crop/'
+    
+    num_of_img=len(files_img)
+    pom=int(num_of_img*pocet_train)
+    
+    for i in range(len(files_img)):
+        
+        image=imread(files_img[i])
+        fov=imread((files_fov[i])).astype(bool)
+        mask_disc=imread(files_mask_disc[i])
+        mask_cup=imread(files_mask_cup[i])
+        
+        if i<pom:
+            center_new=Detection_of_disc(image,fov,sigma,size_of_erosion)
+            output_crop_image, output_mask_disc,output_mask_cup=Crop_image(image,mask_disc,mask_cup,output_image_size,center_new)    
+            output_mask_disc=output_mask_disc.astype(bool)
+            output_mask_cup=output_mask_cup.astype(bool)   
+            
+            plt.imsave(path_to_crop_image + 'Train/' + files_img[i][54:],output_crop_image)
+            plt.imsave(path_to_crop_disc + 'Train/' + files_mask_disc[i][52:],output_mask_disc)
+            plt.imsave(path_to_crop_cup + 'Train/' + files_mask_cup[i][51:],output_mask_cup)
+        else:
+            plt.imsave(path_to_crop_image + 'Test/' + files_img[i][54:],image)
+            plt.imsave(path_to_crop_disc + 'Test/' + files_mask_disc[i][52:],mask_disc)
+            plt.imsave(path_to_crop_cup + 'Test/' + files_mask_cup[i][51:],mask_cup)
+    
+ 
+    
