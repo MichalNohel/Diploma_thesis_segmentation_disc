5524804fa4b5b73e7f857b6be5b4a09087dacefc
Michal Nohel <nohel@PC-SE4158-779.ubmi.feec.vutb.cz>
2022-04-20 13:40:11 +0200

    Hotovy skript na vytvoreni databaze

--- creation_of_crop_images_matlab.m
+++ creation_of_crop_images_matlab.m
@@ -5,8 +5,8 @@ path_to_data='D:\Diploma_thesis_segmentation_disc';
 output_image_size=[500,500];
 sigma=60;
 size_of_erosion=80;
-percentage_number_train=0.8;
-
+percentage_number_test=0.2;
+path_to_crop_image=[path_to_data '/Data_500_500/'];
 
 %% Dristi-GS - Expert 1
 pom=52; % split to test and train dataset
@@ -14,8 +14,6 @@ images_file = dir([path_to_data '\Drishti-GS\Images\*.png']);
 disc_file = dir([path_to_data '\Drishti-GS\Disc\expert1\*.png']);
 cup_file = dir([path_to_data '\Drishti-GS\Cup\expert1\*.png']);
 fov_file = dir([path_to_data '\Drishti-GS\Fov\*.png']);
-path_to_crop_image=[path_to_data '/Data_500_500/'];
-
 
 coordinates_dristi_GS=load('C:\Users\nohel\Desktop\Databaze_final\Drishti-GS\coordinates_dristi_GS.mat');
 coordinates=coordinates_dristi_GS.coordinates_dristi_GS;
@@ -30,8 +28,6 @@ images_file = dir([path_to_data '\REFUGE\Images\Train\*.png']);
 disc_file = dir([path_to_data '\REFUGE\Disc\Train\*.png']);
 cup_file = dir([path_to_data '\REFUGE\Cup\Train\*.png']);
 fov_file = dir([path_to_data '\REFUGE\Fov\Train\*.png']);
-path_to_crop_image=[path_to_data '/Data_500_500/'];
-
 
 coordinates_REFUGE_Train=load('C:\Users\nohel\Desktop\Databaze_final\REFUGE\coordinates_REFUGE_Train.mat');
 coordinates=coordinates_REFUGE_Train.coordinates_REFUGE_Train;
@@ -46,7 +42,6 @@ images_file = dir([path_to_data '\REFUGE\Images\Validation\*.png']);
 disc_file = dir([path_to_data '\REFUGE\Disc\Validation\*.png']);
 cup_file = dir([path_to_data '\REFUGE\Cup\Validation\*.png']);
 fov_file = dir([path_to_data '\REFUGE\Fov\Validation\*.png']);
-path_to_crop_image=[path_to_data '/Data_500_500/'];
 
 
 coordinates_REFUGE_Validation=load('C:\Users\nohel\Desktop\Databaze_final\REFUGE\coordinates_REFUGE_Validation.mat');
@@ -62,7 +57,6 @@ images_file = dir([path_to_data '\REFUGE\Images\Test\*.png']);
 disc_file = dir([path_to_data '\REFUGE\Disc\Test\*.png']);
 cup_file = dir([path_to_data '\REFUGE\Cup\Test\*.png']);
 fov_file = dir([path_to_data '\REFUGE\Fov\Test\*.png']);
-path_to_crop_image=[path_to_data '/Data_500_500/'];
 
 
 coordinates_REFUGE_Test=load('C:\Users\nohel\Desktop\Databaze_final\REFUGE\coordinates_REFUGE_Test.mat');
@@ -72,6 +66,118 @@ pom=401; % split to test and train dataset
 %%
 creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
 
+%% Riga - Bin Rushed
+
+images_file = dir([path_to_data '\RIGA\Images\BinRushed\*.png']);
+disc_file = dir([path_to_data '\RIGA\Disc\BinRushed\expert1\*.png']);
+cup_file = dir([path_to_data '\RIGA\Cup\BinRushed\expert1\*.png']);
+fov_file = dir([path_to_data '\RIGA\Fov\BinRushed\*.png']);
+
+coordinates_RIGA_BinRushed=load('C:\Users\nohel\Desktop\Databaze_final\RIGA\coordinates_RIGA_BinRushed.mat');
+coordinates=coordinates_RIGA_BinRushed.coordinates_RIGA_BinRushed;
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+%%
+creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+
+%% Riga - Magrabia
+
+images_file = dir([path_to_data '\RIGA\Images\Magrabia\*.png']);
+disc_file = dir([path_to_data '\RIGA\Disc\Magrabia\expert1\*.png']);
+cup_file = dir([path_to_data '\RIGA\Cup\Magrabia\expert1\*.png']);
+fov_file = dir([path_to_data '\RIGA\Fov\Magrabia\*.png']);
+
+coordinates_RIGA_Magrabia=load('C:\Users\nohel\Desktop\Databaze_final\RIGA\coordinates_RIGA_Magrabia.mat');
+coordinates=coordinates_RIGA_Magrabia.coordinates_RIGA_Magrabia;
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+%%
+creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+
+%% Riga - MESSIDOS
+
+images_file = dir([path_to_data '\RIGA\Images\MESSIDOS\*.png']);
+disc_file = dir([path_to_data '\RIGA\Disc\MESSIDOS\expert1\*.png']);
+cup_file = dir([path_to_data '\RIGA\Cup\MESSIDOS\expert1\*.png']);
+fov_file = dir([path_to_data '\RIGA\Fov\MESSIDOS\*.png']);
+
+coordinates_RIGA_MESSIDOR=load('C:\Users\nohel\Desktop\Databaze_final\RIGA\coordinates_RIGA_MESSIDOR.mat');
+coordinates=coordinates_RIGA_MESSIDOR.coordinates_RIGA_MESSIDOR;
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+%%
+creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+
+%% RIM-ONE - Glaucoma
+
+images_file = dir([path_to_data '\RIM-ONE\Images\Glaucoma\*.png']);
+disc_file = dir([path_to_data '\RIM-ONE\Disc\Glaucoma\*.png']);
+cup_file = dir([path_to_data '\RIM-ONE\Cup\Glaucoma\*.png']);
+fov_file = dir([path_to_data '\RIM-ONE\Fov\Glaucoma\*.png']);
+
+coordinates_RIM_ONE_Glaucoma=load('C:\Users\nohel\Desktop\Databaze_final\RIM-ONE\coordinates_RIM_ONE_Glaucoma.mat');
+coordinates=coordinates_RIM_ONE_Glaucoma.coordinates_RIM_ONE_Glaucoma;
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+%%
+creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+
+%% RIM-ONE - Healthy
+
+images_file = dir([path_to_data '\RIM-ONE\Images\Healthy\*.png']);
+disc_file = dir([path_to_data '\RIM-ONE\Disc\Healthy\*.png']);
+cup_file = dir([path_to_data '\RIM-ONE\Cup\Healthy\*.png']);
+fov_file = dir([path_to_data '\RIM-ONE\Fov\Healthy\*.png']);
+
+coordinates_RIM_ONE_Healthy=load('C:\Users\nohel\Desktop\Databaze_final\RIM-ONE\coordinates_RIM_ONE_Healthy.mat');
+coordinates=coordinates_RIM_ONE_Healthy.coordinates_RIM_ONE_Healthy;
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+%%
+creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+
+%% UoA_DR - Healthy
+
+images_file = dir([path_to_data '\UoA_DR\Images\Healthy\*.png']);
+disc_file = dir([path_to_data '\UoA_DR\Disc\Healthy\*.png']);
+cup_file = dir([path_to_data '\UoA_DR\Cup\Healthy\*.png']);
+fov_file = dir([path_to_data '\UoA_DR\Fov\Healthy\*.png']);
+
+coordinates_UoA_Healthy=load('C:\Users\nohel\Desktop\Databaze_final\UoA_DR\coordinates_UoA_Healthy.mat');
+coordinates=coordinates_UoA_Healthy.coordinates_UoA_Healthy;
+num_of_img=length(images_file);
+pom=round(num_of_img*percentage_number_test); % split to test and train dataset
+%%
+creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+
+%%
+% %% UoA_DR - NDPR
+% 
+% images_file = dir([path_to_data '\UoA_DR\Images\NDPR\*.png']);
+% disc_file = dir([path_to_data '\UoA_DR\Disc\NDPR\*.png']);
+% cup_file = dir([path_to_data '\UoA_DR\Cup\NDPR\*.png']);
+% fov_file = dir([path_to_data '\UoA_DR\Fov\NDPR\*.png']);
+% 
+% coordinates_UoA_NDPR=load('C:\Users\nohel\Desktop\Databaze_final\UoA_DR\coordinates_UoA_NDPR.mat');
+% coordinates=coordinates_UoA_NDPR.coordinates_UoA_NDPR;
+% num_of_img=length(images_file);
+% pom=round(num_of_img*percentage_number_train); % split to test and train dataset
+% %%
+% creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
+% 
+% %% UoA_DR - PDR
+% 
+% images_file = dir([path_to_data '\UoA_DR\Images\PDR\*.png']);
+% disc_file = dir([path_to_data '\UoA_DR\Disc\PDR\*.png']);
+% cup_file = dir([path_to_data '\UoA_DR\Cup\PDR\*.png']);
+% fov_file = dir([path_to_data '\UoA_DR\Fov\PDR\*.png']);
+% 
+% coordinates_UoA_PDR=load('C:\Users\nohel\Desktop\Databaze_final\UoA_DR\coordinates_UoA_PDR.mat');
+% coordinates=coordinates_UoA_PDR.coordinates_UoA_PDR;
+% num_of_img=length(images_file);
+% pom=round(num_of_img*percentage_number_train); % split to test and train dataset
+% %%
+% creation_of_crop_images(output_image_size,images_file,disc_file,cup_file,fov_file,sigma,size_of_erosion,coordinates,path_to_crop_image,pom)
 
 
 %% Functions
@@ -106,7 +212,7 @@ function [output_crop_image, output_mask_disc,output_mask_cup]=Crop_image(image,
     if ((center_new(1)-y_half)<0)
         y_start=1;
     elseif ((center_new(1)+y_half)>size_in_img(2))
-        y_start=size_in_img(0)-output_image_size(2);
+        y_start=size_in_img(2)-output_image_size(2);
     else
         y_start=center_new(1)-y_half;
     end
